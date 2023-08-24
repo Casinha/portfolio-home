8594ec4a9e6bd010ab31c4e16fc74fcd9b5216cd
Casinha <thehighredeemer@hotmail.com>
2023-08-24 18:12:22 +0100

    Still fixing images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {}
 
 if(process.env.GITHUB_ACTIONS || false) {
     nextConfig.output = "export"
+    nextConfig.basePath = "/portfolio-home"
+    nextConfig.assetPrefix = "/portfolio-home/"
 }
 
 module.exports = nextConfig
