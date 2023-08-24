cc703b1b0d281eed72cb4af134935aefdb4076e1
Casinha <thehighredeemer@hotmail.com>
2023-08-24 02:39:41 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <main className="min-h-screen flex justify-center items-start">
-      <div className="flex flex-col items-center justify-between p-24 max-w-screen-md typewriter">
+      <div className="flex flex-col items-center justify-between p-24 max-w-screen-sm typewriter">
         <h1 className="text-4xl">Wyn&apos;s Portfolio</h1>
         <p className="my-10">Welcome to the home page of my portfolio. Here I list links and descriptions of my portfolio projects for easy access and understanding, most of which are available on <a href="https://github.com/Casinha" target="_blank">Github</a>. <br /><br /> This site - which I have built with a simple, almost minimalist aesthetic reminiscent of typewritten documents or restaurant menus in mind - was built using Next.JS, with some input from copilot.<br/><br/>If you are interested, feel free to get in touch with me via <a href="mailto: wyndb@hotmail.com">email</a>.</p>
         {projectElements}
