2510db5d833bb4546156c6d046b29354c695d472
Casinha <thehighredeemer@hotmail.com>
2023-08-24 18:04:08 +0100

    More fixes to next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {}
 
 if(process.env.GITHUB_ACTIONS || false) {
-    nextConfig.output = "export",
+    nextConfig.output = "export"
+    nextConfig.basePath = "/portfolio-home"
     nextConfig.assetPrefix = "/portfolio-home/"
 }
 
